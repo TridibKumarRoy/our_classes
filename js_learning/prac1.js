3ed3a5fbf8a1d5a0538b564a5cd820a09d5909a5
TridibKumarRoy <118563573+TridibKumarRoy@users.noreply.github.com>
2023-07-10 09:06:57 +0530

    a

---
+++ js_learning/prac1.js
@@ -0,0 +1,26 @@
+console.log("hello world");
+
+{
+    var a = 100 
+}
+let b = "tree"
+// b = 140
+const c = 23.78
+// c = 30   // not possible
+console.log(typeof a)
+console.log(b)
+console.log(c)
+
+
+// primitive data type
+
+let d = null 
+let e = 34  // number
+let f = true  // boolean
+let g = BigInt("56")    // bigint
+console.log(typeof g)
+let h = "hello"     //string
+let i = Symbol("i am a student")    // symbol
+console.log(i)
+let j = undefined
+console.log(j)
