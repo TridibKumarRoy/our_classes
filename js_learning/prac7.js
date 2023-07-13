f8c9cd8f447805b89b8bd6bfbc8b853b275a671b
TridibKumarRoy <118563573+TridibKumarRoy@users.noreply.github.com>
2023-07-13 09:21:48 +0530

    sv

---
+++ js_learning/prac7.js
@@ -0,0 +1,12 @@
+// function
+
+function sqr(num,pow){
+    let pw_val = num**pow
+    return pw_val
+}
+
+let sqr2 = (num,pow)=>{
+    return num**pow
+}
+
+console.log(sqr(2,5))
---
+++ js_learning/prac8.js
@@ -0,0 +1,23 @@
+let s1 = "This is a String"
+console.log(s1)
+console.log(s1[3])
+console.log(s1.length)
+console.log(`${s1} not char`)
+console.log(s1.toUpperCase())
+console.log(s1.toLowerCase())
+console.log(s1.replace("a","the"))
+console.log(s1.slice(10,16))
+console.log(s1.slice(10))
+console.log(s1.startsWith("T"))
+console.log(s1.endsWith("ing"))
+console.log(s1.substring(10,16))
+console.log(s1.concat(" skjsd"))
+console.log(s1.trim().concat(' hah'))
+console.log(s1.charAt(2))
+console.log(s1.charCodeAt(2))
+let arr = s1.split(" ")
+console.log(arr)
+
+
+
+
